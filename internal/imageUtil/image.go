5acdd8430760899939c5d24aec78b682c996c333
rajae <rgivans9@gmail.com>
2025-12-25 18:38:14 -0500

    refactor: Change Loading and encoding implementation

--- internal/imageUtil/image.go
+++ internal/imageUtil/image.go
@@ -2,32 +2,45 @@ package imageutil
 
 import (
 	"encoding/base64"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-func LoadAndEncodeImage(path string) (string, string) {
+func LoadAndEncodeImage(path string) (string, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return "", ""
+		return "", err
 	}
 
 	encoded := base64.StdEncoding.EncodeToString(data)
-	ext := getExtension(path)
 
-	imageURL := "data:image/" + ext + ";base64," + encoded
-	return ext, imageURL
+	return encoded, err
 }
 
-func getExtension(file string) string {
+func GetExtension(file string) string {
 	return strings.TrimPrefix(filepath.Ext(file), ".")
 }
 
-func Rename(oldName string, newName string, ext string) {
+func IsImage(file string) bool {
+	ext := GetExtension(file)
+
+	switch ext {
+	case ".png", ".jpg":
+		return false
+	default:
+		return true
+	}
+}
+
+func Rename(oldName string, newName string, ext string) error {
+	fmt.Println("Renaming File...")
 	err := os.Rename(oldName, newName+"."+ext)
 
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	return nil
 }
