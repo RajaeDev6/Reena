e7d19a29e0aac68741bb57c9672d5d96f69e8407
rajae <rgivans9@gmail.com>
2025-12-25 18:36:26 -0500

    refactor: Change package name to provider, Create Response Object Type

--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -1,4 +1,4 @@
-package openai
+package provider
 
 import (
 	"encoding/json"
@@ -6,31 +6,71 @@ import (
 	"io"
 	"net/http"
 	"strings"
+
+	imageutil "github.com/RajaeDev6/reena/internal/imageUtil"
 )
 
 type OpenAIClient struct {
 	APIKey string
 }
 
-func (c *OpenAIClient) GenerateFilename(imageURL string) (string, error) {
+func NewOpenAIGenerator(APIKey string) *OpenAIClient {
+	return &OpenAIClient{
+		APIKey: APIKey,
+	}
+}
+
+type Response struct {
+	Output []struct {
+		Content []struct {
+			Type string `json:"type"`
+			Text string `json:"text"`
+		} `json:"content"`
+	} `json:"output"`
+}
+
+type ErrorResponse struct {
+	Error struct {
+		Message string `json:"message"`
+		Type    string `json:"type"`
+		Code    string `json:"code"`
+	} `json:"error"`
+}
+
+func buildRequestBody(imageURL string) string {
+	return fmt.Sprintf(`{
+	  "model": "gpt-4.1-mini",
+	  "input": [
+	    {
+	      "role": "user",
+	      "content": [
+	        {
+	          "type": "input_text",
+	          "text": "Look at this image and return a short descriptive filename that matches whats inside the image. Use exactly 3 words. Rules: lowercase only, underscores instead of spaces, no file extension, no punctuation. Return only the name."
+	        },
+	        {
+	          "type": "input_image",
+	          "image_url": "%s"
+	        }
+	      ]
+	    }
+	  ]
+	}`, imageURL)
+}
+
+func (c *OpenAIClient) GenerateFilename(path string) (string, error) {
 	client := &http.Client{}
 
-	reqBody := fmt.Sprintf(`{
-	  "model": "gpt-4.1-mini",
-	  "input": [{
-	    "role": "user",
-	    "content": [
-	      {
-	        "type": "input_text",
-	        "text": "Look at this image and return a short descriptive filename that matches whats inside the image use exactly 3 words. Rules: lowercase only, underscores instead of spaces, no file extension, 3 words, no punctuation, return only the name."
-	      },
-	      {
-	        "type": "input_image",
-	        "image_url": "%s"
-	      }
-	    ]
-	  }]
-	}`, imageURL)
+	encoded, err := imageutil.LoadAndEncodeImage(path)
+	if err != nil {
+		return "", err
+	}
+
+	ext := imageutil.GetExtension(path)
+
+	imageURL := "data:image/" + ext + ";base64," + encoded
+
+	reqBody := buildRequestBody(imageURL)
 
 	req, err := http.NewRequest(
 		"POST",
@@ -55,14 +95,24 @@ func (c *OpenAIClient) GenerateFilename(imageURL string) (string, error) {
 		return "", err
 	}
 
-	return extractFilename(body), nil
-}
-
-func extractFilename(body []byte) string {
-	var data map[string]any
-	if err := json.Unmarshal(body, &data); err != nil {
-		panic(err)
+	if resp.StatusCode != http.StatusOK {
+		var er ErrorResponse
+		if err := json.Unmarshal(body, &er); err != nil {
+			return "", fmt.Errorf("openai error: %s", string(body))
+		}
+		return "", fmt.Errorf("openai error: %s", er.Error.Message)
 	}
 
-	return data["output"].([]any)[0].(map[string]any)["content"].([]any)[0].(map[string]any)["text"].(string)
+	var r Response
+	if err := json.Unmarshal(body, &r); err != nil {
+		return "", err
+	}
+
+	if len(r.Output) == 0 ||
+		len(r.Output[0].Content) == 0 ||
+		r.Output[0].Content[0].Text == "" {
+		return "", fmt.Errorf("empty model output")
+	}
+
+	return r.Output[0].Content[0].Text, nil
 }
